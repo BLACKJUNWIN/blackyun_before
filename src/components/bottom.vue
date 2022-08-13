221c9548c3e4aa0cda54da1c3688a4f74ca02ae4
彭骏豪/BlackJun <1328464063@qq.com>
2022-08-13 16:29:52 +0800

    基础项目

---
+++ src/components/bottom.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="background-color: #202a42;color: white;text-align: center;height:100px;width: 100%;line-height: 100px">
+    2022 © BlackJun.com
+    <a href="https://beian.miit.gov.cn/" target="_blank" style="color: white;text-decoration: none;margin-left: 30px">湘ICP备2021009442号-1</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "bottom"
+}
+</script>
+
+<style scoped>
+
+</style>
